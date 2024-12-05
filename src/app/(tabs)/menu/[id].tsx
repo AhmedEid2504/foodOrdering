46a5d8062cbd5977a9451c0726ff0c139ae6b59e
AhmedEid2504 <ahmedeid2504@gmail.com>
2024-12-05 21:01:03 +0200

    adds layout for the product details screen ready for adding to cart

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,21 +1,76 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import products from '@assets/data/products';
 import { Stack } from 'expo-router';
+import Button from '@components/Button';
 
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetail = () => {
+    const [selectedSize, setSelectedSize] = useState(sizes[1]);
 
     const { id } = useLocalSearchParams();
+    const product = products.find((product) => product.id.toString() === id);
+    
+    const addToCart = () => {
+        alert(`Added ${product.name} to cart in size ${selectedSize}`);
+    }
 
+    if (!product) {
+        return (
+            <View style={styles.wrapper}>
+                <Stack.Screen 
+                    options={{ title: 'Product Detail' }}
+                />
+                <View style={styles.container}>
+                    <Text>Product not found</Text>
+                </View>
+            </View>
+        );
+    }
+    
     return (
         <View style={styles.wrapper}>
             <Stack.Screen 
-                options={{ title: 'Details ' + id }}
+                options={{ title: product ? product.name : 'Product Detail' }}
             />
             <View style={styles.container}>
-                <Text>Product Detail: {id}</Text>
+                <Image 
+                    style={styles.image} 
+                    source={{ uri: product.image }} 
+                    resizeMode='contain'
+                />
+                <Text style={{ fontSize: 20 }}>Select Size</Text>
+                <View style={styles.sizes}>
+                    {sizes.map((size) => (
+                        <Pressable 
+                            key={size}
+                            style={{
+                                ...styles.size,
+                                backgroundColor: size === selectedSize ? 'orange' : 'white',
+                            }}
+                            onPress={() => setSelectedSize(size)}
+                        >
+                            <Text 
+                                style={{
+                                    ...styles.sizeText,
+                                    color: size === selectedSize ? 'red' : 'black',
+                                }}
+                            >
+                                {size}
+                            </Text>
+                        </Pressable>
+                    ))}
+                </View>
+                <View style={styles.bottomContainer}>
+                    <Text style={styles.title}>{product.name}</Text>
+                    <Text style={styles.title}>${product.price}</Text>
+                    <Button 
+                        text="Add to cart" 
+                        onPress={addToCart}
+                    />
+                </View>
             </View>
         </View>
     );
@@ -32,7 +87,42 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: 'white',
         borderRadius: 10,
+        padding: 10,
         width: '100%',
+        justifyContent: 'space-around',
+    },
+    image: {
+        width: '100%',
+        aspectRatio: 1,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+    sizes: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        gap: 10,
+        padding: 10,
+    },
+    size: {
+        width: 50,
+        aspectRatio: 1,
+        borderRadius: 25,
+        borderColor: 'darkorange',
+        borderWidth: 4,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    sizeText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    bottomContainer: {
+        padding: 10,
+        alignItems: 'center',
     },
 });
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,36 @@
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { forwardRef } from 'react';
+
+type ButtonProps = {
+    text: string;
+} & React.ComponentPropsWithoutRef<typeof Pressable>;
+
+const Button = forwardRef<View | null, ButtonProps>(
+    ({ text, ...pressableProps }, ref) => {
+        return (
+        <Pressable ref={ref} {...pressableProps} style={styles.container}>
+            <Text style={styles.text}>{text}</Text>
+        </Pressable>
+        );
+    }
+);
+
+const styles = StyleSheet.create({
+    container: {
+        width: 200,
+        backgroundColor: 'white',
+        borderWidth: 4,
+        borderColor: 'darkorange',
+        padding: 15,
+        alignItems: 'center',
+        borderRadius: 100,
+        marginVertical: 10,
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: '600',
+        color: 'black',
+    },
+});
+
+export default Button;
